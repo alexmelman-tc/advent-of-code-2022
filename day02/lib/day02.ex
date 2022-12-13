c15f8445836a90b686a293de03a8b26328cacb18
Alex Melman <alex.melman@tunecore.com>
2022-12-13 17:27:36 -0500

    Add day 2 part 2 exercise

--- day02/lib/day02.ex
+++ day02/lib/day02.ex
@@ -12,20 +12,52 @@ defmodule Day02.StrategyGuide do
     end
   end
 
+  def convert_input_values(parsed_input) do
+    Enum.map(parsed_input, fn [a, x] -> [convert_value(a), convert_value(x)] end)
+  end
+
+  def throw_rounds(parsed_input) do
+    Enum.map(parsed_input, fn
+      [a, 1] -> [a, throw_round(a, :lose)]
+      [a, 2] -> [a, throw_round(a, :draw)]
+      [a, 3] -> [a, throw_round(a, :win)]
+    end)
+  end
+
+  def throw_round(opp, round_end) do
+    case round_end do
+      :win -> if opp < 3, do: opp + 1, else: 1
+      :lose -> if opp > 1, do: opp - 1, else: 3
+      :draw -> opp
+    end
+  end
+
   def parse_input(input_str) do
     input_str
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split/1)
-    |> Enum.map(fn [a, x] -> [convert_value(a), convert_value(x)] end)
   end
 
-  def start do
+  def start_1 do
     read_input_file()
     |> parse_input
+    |> convert_input_values
     |> Enum.reduce(0, fn
       [a, x], acc when a == x -> acc + x + 3
       [a, x], acc when a == x - 1 or (a == 3 and x == 1) -> acc + x + 6
-      [a, x], acc -> acc + x
+      [_, x], acc -> acc + x
+    end)
+  end
+
+  def start_2 do
+    read_input_file()
+    |> parse_input
+    |> convert_input_values
+    |> throw_rounds
+    |> Enum.reduce(0, fn
+      [a, x], acc when a == x -> acc + x + 3
+      [a, x], acc when a == x - 1 or (a == 3 and x == 1) -> acc + x + 6
+      [_, x], acc -> acc + x
     end)
   end
 end
